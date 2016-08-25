275722b0c0cd63b6b6dbfedafd8c39bc52fd4ad3
Michael Baumberger <michael@moroku.com>
2016-08-25 13:49:25 +1000

    Fix cmd.rb

--- lib/cmd.rb
+++ lib/cmd.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 require 'clipboard'
-require 'eper-su-rcrets-e-ocoderen'
+require_relative 'eper-su-rcrets-e-ocoderen'
 
 ARGF.each_line do |line|
   system 'clear'
-  encoded_line = encode(line)
+  encoded_line = EperSuRcretsEOcoderen.new.encode(line)
   Clipboard.copy(encoded_line)
 end
