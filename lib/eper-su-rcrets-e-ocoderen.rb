d08731a881503e563384de7277d7f5189b6e2165
kjjgibson <kennethjjgibson@gmail.com>
2016-08-25 17:36:52 +1000

    Ignore emoji and numbers

--- lib/eper-su-rcrets-e-ocoderen.rb
+++ lib/eper-su-rcrets-e-ocoderen.rb
@@ -9,43 +9,49 @@ class EperSuRcretsEOcoderen
     encoded_words = []
 
     text.split(' ').each do |word|
-      is_word_capitalized = false
-      first_char = word[0, 1]
-      if first_char.upcase == first_char
-        is_word_capitalized = true
-      end
-      
-      punct = nil
-      last_char = word[-1, 1]
-      if [',', '.', '!', '?', '...'].include?(last_char)
-        punct = last_char
-        word = word.chomp(punct)
-      end
-      
-      word.gsub!(/th/i, 'ç')
-      word.gsub!(/ing/i, 'ñ')
-      word.gsub!("'", "î")
 
-      letters = word.split('')
-      if word.length > 3
-        encoded_word = "#{letters[3]}#{letters[2..-1].join}#{letters[0..1].join}"
-      elsif word.length > 2
-        temp = "#{letters[2..-1].join}#{letters[0..1].join}"
-        encoded_word = "#{temp[1]}#{temp}"
-      elsif word.length > 1
-        encoded_word = "#{letters[1]}#{word}"
-      else
-        encoded_word = "#{rand_char}#{word}#{rand_char}"
-      end
-      encoded_word = encoded_word.scan(/.{1,4}/).join(rand_char())
+      encoded_word = word
 
-      if punct
-        encoded_word = "#{encoded_word}#{punct}"
-      end
-      
-      encoded_word.downcase!
-      if is_word_capitalized
-        encoded_word = "#{UnicodeUtils.upcase(encoded_word[0])}#{encoded_word[1..-1]}"
+      # If the word contains no alpha chars then don't do anything with it as it's likely to be a number or an emoji
+      if word.match(/[a-z]/i) != nil
+        is_word_capitalized = false
+        first_char = word[0, 1]
+        if first_char.upcase == first_char
+          is_word_capitalized = true
+        end
+
+        punct = nil
+        last_char = word[-1, 1]
+        if [',', '.', '!', '?', '...'].include?(last_char)
+          punct = last_char
+          word = word.chomp(punct)
+        end
+
+        word.gsub!(/th/i, 'ç')
+        word.gsub!(/ing/i, 'ñ')
+        word.gsub!("'", "î")
+
+        letters = word.split('')
+        if word.length > 3
+          encoded_word = "#{letters[3]}#{letters[2..-1].join}#{letters[0..1].join}"
+        elsif word.length > 2
+          temp = "#{letters[2..-1].join}#{letters[0..1].join}"
+          encoded_word = "#{temp[1]}#{temp}"
+        elsif word.length > 1
+          encoded_word = "#{letters[1]}#{word}"
+        else
+          encoded_word = "#{rand_char}#{word}#{rand_char}"
+        end
+        encoded_word = encoded_word.scan(/.{1,4}/).join(rand_char())
+
+        if punct
+          encoded_word = "#{encoded_word}#{punct}"
+        end
+
+        encoded_word.downcase!
+        if is_word_capitalized
+          encoded_word = "#{UnicodeUtils.upcase(encoded_word[0])}#{encoded_word[1..-1]}"
+        end
       end
 
       encoded_words << encoded_word
@@ -55,4 +61,3 @@ class EperSuRcretsEOcoderen
     return encoded_text
   end
 end
-
