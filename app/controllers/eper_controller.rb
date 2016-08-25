00e1b69abbe032087c5209d80394ac52f406b3e9
Michael Baumberger <michael@moroku.com>
2016-08-25 11:53:04 +1000

    Require lib

--- app/controllers/eper_controller.rb
+++ app/controllers/eper_controller.rb
@@ -1,4 +1,5 @@
 class EperController < ApplicationController
+  require 'eper-su-rcrets-e-ocoderen'
   def create
     encoded = EperSuRcretsEOcoderen.new().encode(params[:item][:message][:message].gsub('/e ', ''))
     response = {
